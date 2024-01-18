255a348fe7e383123d41a47f2a3f9bc2ef0c4ec2
vishnu <vishnsingh007@gmail.com>
2024-01-18 19:06:59 +0530

    add a generice response method

--- app/services/user-service/handlers/v1/users/user.go
+++ app/services/user-service/handlers/v1/users/user.go
@@ -2,8 +2,9 @@ package users
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
+
+	"github.com/vishn007/go-service-template/foundation/web"
 )
 
 // Handlers manages the set of user endpoints.
@@ -20,17 +21,5 @@ func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		Status: "OK OK",
 	}
 
-	jsonData, err := json.Marshal(status)
-	if err != nil {
-		return err
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	if _, err := w.Write(jsonData); err != nil {
-		return err
-	}
-
-	return nil
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
