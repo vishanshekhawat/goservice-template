08ec671771038dc5a6a6c37c4eaf654b542d7696
vishnu <vishnsingh007@gmail.com>
2024-01-30 12:57:01 +0530

    database dependency in progress

--- buisness/repo/usercache.go
+++ buisness/repo/usercache.go
@@ -5,19 +5,18 @@ import "database/sql"
 type UserCache struct {
 }
 
-// Connect initializes a connection to the PostgreSQL database.
+// Connect initializes a connection to the in-memory cache.
 func (p *UserCache) Connect() error {
-
 	return nil
 }
 
-// Close closes the connection to the PostgreSQL database.
+// Close closes the connection to the the in-memory cache.
 func (p *UserCache) Close() error {
 
 	return nil
 }
 
-// Close closes the connection to the PostgreSQL database.
+// Close closes the connection to the in-memory cache.
 func (p *UserCache) GetConn() *sql.DB {
 	return nil
 }
